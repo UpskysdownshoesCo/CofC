ceab79dd5037ee996c690ab13685ee0342bd8588
upskydownshoes <midean777@gmail.com>
2024-01-15 18:27:11 +0000

    Take that.

--- ui/public/settings.js
+++ ui/public/settings.js
@@ -2,7 +2,7 @@
 // (to obtain access tokens for the API)
 window.Settings = {
   api_url: "",
-  api_scope: "",
-  client_id: "",
-  client_authority: "",
+  api_scope: "api://a96791c9-7ccc-4039-a7d0-3603d8b83b1d/Files.Read",
+  client_id: "5a038fd9-4b6e-41f3-b413-042af1499d23",
+  client_authority: "https://login.microsoftonline.com/1e1e11de-a49b-4717-a2d9-a7a68529516d",
 };
